1388c1e9914652578b6cfd09c04d0d31827c3b77
Konstantin15101997 <dikarykostya@gmail.com>
2024-02-10 13:25:47 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#include <ESP8266WiFi.h>
+/*#define ESP8266
+#ifdef ESP32
+#include <WiFi.h>
+#else 
+#include <ESP8266WiFi.h>
+#endif*/
+ 
+void setup() { 
+  Serial.begin(115200);
+  Serial.println(); 
+  Serial.print("ESP Board MAC Address:&nbsp; "); 
+  Serial.println(WiFi.macAddress());
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
