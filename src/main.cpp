a7cea30a0926c1ac20a48711357cba6004665b51
Konstantin15101997 <dikarykostya@gmail.com>
2024-02-10 15:15:27 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,86 @@
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-/*#define ESP8266
-#ifdef ESP32
-#include <WiFi.h>
-#else 
-#include <ESP8266WiFi.h>
-#endif*/
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-one-to-many-esp8266-nodemcu/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
  
-void setup() { 
-  Serial.begin(115200);
-  Serial.println(); 
-  Serial.print("ESP Board MAC Address:&nbsp; "); 
-  Serial.println(WiFi.macAddress());
+//Отправитель данных
+
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+ 
+// REPLACE WITH RECEIVER MAC Address
+uint8_t broadcastAddress1[] = {0xE8, 0x9F, 0x6D, 0x72, 0x35, 0xF8};
+ 
+// Structure example to send data
+// Must match the receiver structure
+typedef struct test_struct {
+    int x;
+    int y;
+} test_struct;
+ 
+// Create a struct_message called test to store variables to be sent
+test_struct test;
+ 
+unsigned long lastTime = 0;  
+unsigned long timerDelay = 50;  // send readings timer
+ 
+// Callback when data is sent
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  char macStr[18];
+  Serial.print("Packet to:");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+         mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+  Serial.print(" send status: ");
+  if (sendStatus == 0){
+    Serial.println("Delivery success");
+  }
+  else{
+    Serial.println("Delivery fail");
+  }
 }
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+ 
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+ 
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  esp_now_add_peer(broadcastAddress1, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+ 
+}
+ 
 void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    // Set values to send
+    test.x = Serial.read();
+    test.y = -255;
+ 
+    // Send message via ESP-NOW
+    esp_now_send(0, (uint8_t *) &test, sizeof(test));
+ 
+    lastTime = millis();
+  }
+  
 }
